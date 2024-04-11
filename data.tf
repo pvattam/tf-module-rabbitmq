09bb39ed725bb1c05293b055611e5f8de54e9f9a
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-11 06:59:57 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- data.tf
+++ data.tf
@@ -1,6 +1,12 @@
+#data "aws_ami" "ami" {
+#  most_recent      = true
+#  name_regex       = "golden-ami"
+##  name_regex       = "golden-ami-v01282024"
+#  owners           = ["072976934238"]
+#}
+
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "golden-ami"
-#  name_regex       = "golden-ami-v01282024"
-  owners           = ["072976934238"]
+  most_recent = true
+  name_regex  = "golden-ami"
+  owners      = ["072976934238"]
 }
